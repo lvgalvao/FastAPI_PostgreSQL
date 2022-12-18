223b4ab1d37f4510b25e65fca9ac80133771f850
Luciano Filho <lvgalvaofilho@gmail.com>
2022-12-18 12:16:24 -0300

    updade models

--- app/models.py
+++ app/models.py
@@ -13,9 +13,15 @@ class Role(str, Enum):
     student = "student"
 
 class User(BaseModel):
-    id: Optional[UUID] = uuid4
+    id: Optional[UUID] = uuid4()
     first_name: str
     last_name: str
     middle_name: Optional[str]
     gender: Gender
     roles: List[Role]
+
+class UserUpdateRequest(BaseModel):
+    first_name: Optional[str]
+    last_name: Optional[str]
+    middle_name: Optional[str]
+    roles: Optional[List[Role]]
